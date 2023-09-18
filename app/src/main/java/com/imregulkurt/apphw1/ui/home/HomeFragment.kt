590758848edc8735d0c6eb0655e7611556f03916
imku13 <imregulkurt@gmail.com>
2023-09-19 01:59:51 +0300

    Navigation home to personal info

--- app/src/main/java/com/imregulkurt/apphw1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/imregulkurt/apphw1/ui/home/HomeFragment.kt
@@ -2,7 +2,9 @@ package com.imregulkurt.apphw1.ui.home
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.imregulkurt.apphw1.R
 import com.imregulkurt.apphw1.common.viewBinding
 import com.imregulkurt.apphw1.databinding.FragmentHomeBinding
@@ -13,5 +15,14 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.homeButton.setOnClickListener {
+            if (binding.homeNameInput.editText?.text?.isEmpty() == true) {
+                Toast.makeText(activity, "Input fields cannot be empty!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                findNavController().navigate(R.id.home_to_personal)
+            }
+        }
     }
 }
